858ec91f9ef66077f6e3cd28baf4d1a11d0c0a03
Adam.Carter <adamkristopher@gmail.com>
2014-08-24 17:13:25 -0500

    Made style changes to banner.

--- inc/banner.php
+++ inc/banner.php
@@ -3,16 +3,15 @@
 	<div class="banner_background">
 
 		<div class="grid-30 banner_logo">
-			<img src="<?php echo $plugin_url . '/images/unicef.png'; ?>">
+			<a href="http://tap.unicefusa.org/" target="_blank"><img src="<?php echo $plugin_url . '/images/unicef.png'; ?>"></a>
 		</div>
 
-		<div class="grid-30 banner_headline">
+		<div class="grid-40 banner_headline">
 			<h1>Every heart needs water</h1>
 		</div>
 
-		<div class="grid-40 banner_button">
-			<a href="https://www.unicefusa.org/donate/donate-unicef-tap-project/16034?pcode=WB_XXXTAP15YXXXX" target="_blank"><button type="button">Donate</button></a>
-		</div>
+		<a href="https://www.unicefusa.org/donate/donate-unicef-tap-project/16034?pcode=WB_XXXTAP15YXXXX" target="_blank">
+		<div class="grid-20 banner_button">Donate</div></a>
 
 	</div>
 
