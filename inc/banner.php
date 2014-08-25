b5e8b6bae4d8f317c193d9e066f0041bd0935298
Adam.Carter <adamkristopher@gmail.com>
2014-08-25 13:42:11 -0500

    Fixed some stlye issues.

--- inc/banner.php
+++ inc/banner.php
@@ -1,23 +1,12 @@
-<div class="background" style="background-color:<?php echo $background_color; ?>;">
+<div class="utp_banner_body" style="background-color:<?php echo $background_color; ?>;">
 
-	<div class="banner_background">
+	<a href="http://tap.unicefusa.org/" target="_blank">
+	<?php echo '<img src="' . plugins_url( 'images/unicef.png' , dirname(__FILE__) ) . '" > '; ?></a>
 
-		<div class="banner_logo">
-			<a href="http://tap.unicefusa.org/" target="_blank"><?php echo '<img src="' . plugins_url( 'images/unicef.png' , dirname(__FILE__) ) . '" > '; ?></a>
-		</div>
-
-		<div class="banner_headline">
-			<h1 style="color:<?php echo $headline_color; ?>;">Every heart needs water</h1>
-		</div>
-
-		<a href="https://www.unicefusa.org/donate/donate-unicef-tap-project/16034?pcode=WB_XXXTAP15YXXXX" target="_blank">
-		<div class="banner_button" style="color:<?php echo $button_color; ?>; border: 2px solid <?php echo $button_color; ?>;">Donate</div></a>
-
-		<style type="text/css">.banner_button:hover{ color: #fff; background-color: <?php echo $button_color; ?>; }</style>
-
+	<div class="utp_banner_button" style="color:<?php echo $button_color; ?>; border: 2px solid <?php echo $button_color; ?>;">
+		<style type="text/css">.utp_banner_button:hover{ color: #fff; background-color: <?php echo $button_color; ?>; }</style>
+		<a href="https://www.unicefusa.org/donate/donate-unicef-tap-project/16034?pcode=WB_XXXTAP15YXXXX" target="_blank">Donate</a>
 	</div>
 
 </div>
-
-
 
