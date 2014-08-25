af4fd0536248ef0409f8e82cb23bafc9eaf8ad64
Adam.Carter <adamkristopher@gmail.com>
2014-08-25 12:23:51 -0500

    Removing more grid leftovers.

--- inc/banner.php
+++ inc/banner.php
@@ -1,17 +1,17 @@
 <div class="background" style="background-color:<?php echo $background_color; ?>;">
 
-	<div class="grid-container banner_background">
+	<div class="banner_background">
 
-		<div class="grid-30 banner_logo">
+		<div class="banner_logo">
 			<a href="http://tap.unicefusa.org/" target="_blank"><?php echo '<img src="' . plugins_url( 'images/unicef.png' , dirname(__FILE__) ) . '" > '; ?></a>
 		</div>
 
-		<div class="grid-40 banner_headline">
+		<div class="banner_headline">
 			<h1 style="color:<?php echo $headline_color; ?>;">Every heart needs water</h1>
 		</div>
 
 		<a href="https://www.unicefusa.org/donate/donate-unicef-tap-project/16034?pcode=WB_XXXTAP15YXXXX" target="_blank">
-		<div class="grid-20 banner_button" style="color:<?php echo $button_color; ?>; border: 2px solid <?php echo $button_color; ?>;">Donate</div></a>
+		<div class="banner_button" style="color:<?php echo $button_color; ?>; border: 2px solid <?php echo $button_color; ?>;">Donate</div></a>
 
 		<style type="text/css">.banner_button:hover{ color: #fff; background-color: <?php echo $button_color; ?>; }</style>
 
