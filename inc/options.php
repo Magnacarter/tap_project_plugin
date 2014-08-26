3a24d0fd4b536f230df457f30620df9119ea52ad
Adam.Carter <adamkristopher@gmail.com>
2014-08-26 11:41:34 -0500

    Escaped vars and made hardcoded strings translatable on options.php.

--- inc/options.php
+++ inc/options.php
@@ -1,19 +1,19 @@
 
-<div class="utp_banner_body" style="background-color:<?php echo $background_color; ?>;">
+<div class="utp_banner_body" style="background-color:<?php echo esc_attr( $background_color ); ?>;">
 
 	<a href="http://tap.unicefusa.org/" target="_blank">
 	<?php echo '<img src="' . plugins_url( 'images/unicef.png' , dirname(__FILE__) ) . '" > '; ?></a>
 
-	<h1 style="color:<?php echo $headline_color; ?>;">Every heart needs water.</h1>
+	<h1 style="color:<?php echo esc_attr( $headline_color ); ?>;"><?php _e( 'Every heart needs water', 'unicef-tap-project-plugin' ); ?>.</h1>
 
-	<div class="utp_banner_button" style="color:<?php echo $button_color; ?>; border: 2px solid <?php echo $button_color; ?>;">
+	<div class="utp_banner_button" style="color:<?php echo esc_attr( $button_color ); ?>; border: 2px solid <?php echo esc_attr( $button_color ); ?>;">
 	<a href="https://www.unicefusa.org/donate/donate-unicef-tap-project/16034?pcode=WB_XXXTAP15YXXXX" target="_blank">Donate</a></div>
 
-	<style type="text/css">.utp_banner_button:hover{ color: #fff; background-color: <?php echo $button_color; ?>; }</style>
+	<style type="text/css">.utp_banner_button:hover{ color: #fff; background-color: <?php echo esc_attr( $button_color ); ?>; }</style>
 
 </div>
 
-<?php echo '<p style="color:<?php echo $headline_color; ?>;">hello</p>'; ?>
+
 
 
 
