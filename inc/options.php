25849d879f03dbf86b6cc2c74885301e6fea52bb
Adam.Carter <adamkristopher@gmail.com>
2014-08-26 11:56:08 -0500

    Missed a hardcoded string from a previous commit.

--- inc/options.php
+++ inc/options.php
@@ -5,10 +5,10 @@
 		<img src="<?php echo esc_url( UTP_PLUGIN_URL . 'images/unicef.png' ); ?>" />
 	</a>
 
-	<h1 style="color:<?php echo esc_attr( $headline_color ); ?>;"><?php _e( 'Every heart needs water', 'unicef-tap-project-plugin' ); ?>.</h1>
+	<h1 style="color:<?php echo esc_attr( $headline_color ); ?>;"><?php _e( 'Every heart needs water.', 'unicef-tap-project-plugin' ); ?></h1>
 
 	<div class="utp_banner_button" style="color:<?php echo esc_attr( $button_color ); ?>; border: 2px solid <?php echo esc_attr( $button_color ); ?>;">
-	<a href="https://www.unicefusa.org/donate/donate-unicef-tap-project/16034?pcode=WB_XXXTAP15YXXXX" target="_blank">Donate</a></div>
+	<a href="https://www.unicefusa.org/donate/donate-unicef-tap-project/16034?pcode=WB_XXXTAP15YXXXX" target="_blank"><?php _e( 'Donate', 'unicef-tap-project-plugin' ); ?></a></div>
 
 	<style type="text/css">.utp_banner_button:hover{ color: #fff; background-color: <?php echo esc_attr( $button_color ); ?>; }</style>
 
