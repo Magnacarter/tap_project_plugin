7c61bca2d1b54fd9a1fafb7ef4a4c489c5cc844a
Adam.Carter <adamkristopher@gmail.com>
2014-08-25 23:09:58 -0500

    Added the headline back. Still can't get data from option forms.

--- inc/options.php
+++ inc/options.php
@@ -4,6 +4,8 @@
 	<a href="http://tap.unicefusa.org/" target="_blank">
 	<?php echo '<img src="' . plugins_url( 'images/unicef.png' , dirname(__FILE__) ) . '" > '; ?></a>
 
+	<h1>Every heart needs water.</h1>
+
 	<div class="utp_banner_button" style="color:<?php echo $button_color; ?>; border: 2px solid <?php echo $button_color; ?>;">
 	<a href="https://www.unicefusa.org/donate/donate-unicef-tap-project/16034?pcode=WB_XXXTAP15YXXXX" target="_blank">Donate</a></div>
 
